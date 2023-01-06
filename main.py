358653c72c6b6fc3b9466c04fbb14780353abe6c
LKR4N <aptemons000@gmail.com>
2023-01-06 13:39:52 +0200

    final version parse_cookie functions + added 10 test

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from http.cookies import SimpleCookie
 
 
+def parse(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/') == {}
+    assert parse('https://example.com/?') == {}
+    assert parse('https://example.com/?name=Dima') == {'name': 'Dima'}
+
+
 def parse_cookie(query: str) -> dict:
     ps = SimpleCookie()
     ps.load(query)
